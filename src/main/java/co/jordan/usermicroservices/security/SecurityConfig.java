90e50f3c539a2b86f9e1913136b633a524330b28
winigajordan <winigajordan@gmail.com>
2025-03-31 13:05:13 +0000

    JWT Spring - Add CORS Configuration

--- src/main/java/co/jordan/usermicroservices/security/SecurityConfig.java
+++ src/main/java/co/jordan/usermicroservices/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package co.jordan.usermicroservices.security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -22,15 +29,36 @@ public class SecurityConfig {
     {
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf->csrf.disable())
+
+                .cors(cors-> cors.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                       CorsConfiguration cors = new CorsConfiguration();
+
+                       cors.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                       //cors.setAllowedOrigins(Arrays.asList("url1", "url2"));
+                       cors.setAllowedMethods(Collections.singletonList("*"));
+                       cors.setAllowedHeaders(Collections.singletonList("*"));
+                       cors.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                       return  cors;
+                    }
+                }))
+
+
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/login").permitAll()
                         .requestMatchers("/all").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
+
+
                 .addFilterBefore(
                         new JWTAuthenticationFilter(authenticationManager),
                         UsernamePasswordAuthenticationFilter.class
                 )
+
+
                 .addFilterBefore(
                         new JWTAUthorizationFilter(),
                         UsernamePasswordAuthenticationFilter.class
